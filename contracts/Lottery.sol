ef2dc163205ef193748eff39b7af72aae1103d84
Kim Geon-Woo <kwookim@sgacorp.kr>
2019-07-30 11:44:18 +0900

    feat : distribute
    베팅 결과값을 확인하고 팟머니를 분배한다.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -12,6 +12,8 @@ contract Lottery {
     mapping (uint256 => BetInfo) private _bets;
 
     address public owner;
+    bool private mode = false; // false : use answer for test, true : use real block hash
+    bytes32 public answerForTest;
 
     uint256 constant internal BLOCK_LIMIT = 256;
     uint256 constant internal BET_BLOCK_INTERVAL = 3;
@@ -23,6 +25,10 @@ contract Lottery {
     enum BettingResult { Fail, Win, Draw }
 
     event BET(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBetInfo);
+    event WIN(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+    event FAIL(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+    event DRAW(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+    event REFUND(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
 
     constructor() public {
         owner = msg.sender;
@@ -52,12 +58,18 @@ contract Lottery {
         return true;
     }
 
-    // Distribute
+    /**
+     * @dev 베팅 결과값을 확인하고 팟머니를 분배한다.
+     * 정답 실패 : 팟머니 축척, 정답 맞춤 : 팟머니 획득, 한글자 맞춤 or 정답 확인 불가 : 베팅 금액만 획득
+     */
     function distribute() public {
 
         uint256 cur;
+        uint256 transferAmount;
+
         BetInfo memory b;
         BlockStatus currentBlockStatus;
+        BettingResult currentBettingResult;
 
         for(cur = _head; cur < _tail ; cur++) {
             b = _bets[cur];
@@ -65,11 +77,37 @@ contract Lottery {
 
             // Checkable : block.number > answerBlockNumber && block.number < BLOCK_LIMIT + answerBlocknumber
             if (currentBlockStatus == BlockStatus.Checkable) {
+                bytes32 answerBlockHash = getAnswerBlockHash(b.answerBlockNumber);
+                currentBettingResult = isMatch(b.challenges, answerBlockHash);
                 // if win, bettor gets pot
+                if (currentBettingResult == BettingResult.Win) {
+                    // transfer pot
+                    transferAmount = transferAfterPayingFee(b.bettor, _pot + BET_AMOUNT);
+
+                    // pot = 0
+                    _pot = 0;
+
+                    // emit WIN
+                    emit WIN(cur, b.bettor, transferAmount, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+                }
 
                 // if fail, bettor's money goes pot
+                if (currentBettingResult == BettingResult.Fail) {
+                    // pot = 0
+                    _pot += BET_AMOUNT;
+
+                    // emit FAIL
+                    emit FAIL(cur, b.bettor, transferAmount, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+                }
 
                 // if draw, refund bettor's money
+                if (currentBettingResult == BettingResult.Draw) {
+                    // transfer only BET_AMOUNT to bettor
+                    transferAmount = transferAfterPayingFee(b.bettor, BET_AMOUNT);
+
+                    // emit DRAW
+                    emit DRAW(cur, b.bettor, transferAmount, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+                }
             }
 
             // Not Revealed : block.number <= answerBlockNumber
@@ -80,10 +118,39 @@ contract Lottery {
             // Block Limit Passwd : block.number >= answerBlockNumber + BLOCK_LIMIT
             if (currentBlockStatus == BlockStatus.BlockLimitPassed) {
                 // refund
+                transferAmount = transferAfterPayingFee(b.bettor, BET_AMOUNT);
+
                 // emit refund
+                emit REFUND(cur, b.bettor, transferAmount, b.challenges, b.answerBlockNumber);
             }
             popBet(cur);
         }
+        _head = cur;
+    }
+
+    function transferAfterPayingFee(address payable addr, uint256 amount) internal returns (uint256) {
+
+        // uint256 fee = amount / 100;
+        uint256 fee = 0;
+        uint256 amountWithoutFee = amount - fee;
+
+        // transfer to addr
+        addr.transfer(amountWithoutFee);
+
+        // transfer to owner
+        addr.transfer(fee);
+
+        return amountWithoutFee;
+    }
+
+    function setAnswerForTest(bytes32 answer) public returns (bool result) {
+        require(msg.sender == owner, "Only owner can set the answer for test mode");
+        answerForTest = answer;
+        return true;
+    }
+
+    function getAnswerBlockHash(uint256 answerBlockNumber) internal view returns (bytes32 answer) {
+        return mode ? blockhash(answerBlockNumber) : answerForTest;
     }
 
     /**
